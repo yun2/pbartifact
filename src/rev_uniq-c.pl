c4bd6aabf38584438a93f4049325d944d8c1193e
yun2 <imai@cb.k.u-tokyo.ac.jp>
2019-03-23 09:19:05 +0900

    rev_uniq-c.pl: for cases where there is no leading space

--- src/rev_uniq-c.pl
+++ src/rev_uniq-c.pl
@@ -3,9 +3,10 @@ use strict;
 
 while(<>){
   chomp;
+  $_ =~ s/^\s+//;
   my @uniq_c_out = split /\s+/, $_;
   #print "$uniq_c_out[0]\n";
   #print "$uniq_c_out[1]\t$uniq_c_out[2]\n";
-  print "$uniq_c_out[2]\t$uniq_c_out[1]\n";
+  print "$uniq_c_out[1]\t$uniq_c_out[0]\n";
   #print "$uniq_c_out[2]\n";
 }
