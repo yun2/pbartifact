29e8b9f6f5ecc0ee26fda6451d1703bc1f2fff7d
yun2 <imai@cb.k.u-tokyo.ac.jp>
2019-03-23 09:29:02 +0900

    csk4gepardinput.pl: misc

--- src/csk4gepardinput.pl
+++ src/csk4gepardinput.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 
 my $prefix="ecoli.p6c4";
-print "count_shared_kmer -k 24 $prefix.fa > $prefix.csk\n";
-print "cat $prefix.csk | sort -k1,1nr > $prefix.csk.sorted\n";
-print "sharing_kmer_selector.pl -min_hit 75 -max_hit 99 $prefix.csk.sorted | cut -f 2 > $prefix.candidates.list\n";
-print "extract_fa.pl $prefix.fa $prefix.candidates.list > $prefix.candidates.fa\n";
+print "count_shared_kmer -k 24 $prefix.fa > $prefix.fa.csk\n";
+print "cat $prefix.fa.csk | sort -k1,1nr > $prefix.fa.csk.sorted\n";
+print "shared_kmer_selector.pl -min_hit 75 -max_hit 99 $prefix.fa.csk.sorted | cut -f 2 > $prefix.fa.candidates.list\n";
+print "extract_fa.pl $prefix.fa $prefix.fa.candidates.list > $prefix.fa.candidates.fa\n";
 
 #gepard
 
