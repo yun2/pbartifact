6160e23c96cb554fc96720a274e86382a47555a8
yun2 <imai@cb.k.u-tokyo.ac.jp>
2019-03-26 16:41:49 +0900

    misc

--- src/gepard_command_generator.pl
+++ src/gepard_command_generator.pl
@@ -4,6 +4,6 @@ my $prefix="real";
 my $n_fa=62;
 
 for(my $i=0; $i < $n_fa; $i++){
-  printf("./gepardcmd.sh -seq1 ../${prefix}_00%02d.fa -seq2 ../${prefix}_00%02d.fa -matrix ./matrices/edna.mat -outfile ${prefix}_00%02d.png\n",$i,$i,$i);
+  printf("./gepardcmd.sh -seq1 ../${prefix}_%04d.fa -seq2 ../${prefix}_%04d.fa -matrix ./matrices/edna.mat -outfile ${prefix}_%04d.png\n",$i,$i,$i);
 }
 
